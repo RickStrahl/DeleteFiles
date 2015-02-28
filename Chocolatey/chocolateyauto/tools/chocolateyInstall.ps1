62344f688c78c4da24fb0154f3e323e597ee5a10
Rick Strahl <rstrahl@west-wind.com>
2015-02-28 01:33:54 -1000

    Add Zip file to distribution

---
+++ Chocolatey/chocolateyauto/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+$packageName = 'DeleteFiles' # arbitrary name for the package, used in messages
+$url = 'https://github.com/RickStrahl/DeleteFiles/raw/master/Distribution/DeleteFiles.zip' # download url
+$drop = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# main helpers - these have error handling tucked into them already
+# installer, will assert administrative rights
+# if removing $url64, please remove from here
+Install-ChocolateyZipPackage $packageName $url  $drop

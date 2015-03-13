0b8212241de9525f356bc9b9b601a96751976c1b
Rick Strahl <rstrahl@west-wind.com>
2015-03-13 02:16:06 -1000

    Add image and update version for Chocolatey

--- DeleteFiles/Properties/AssemblyInfo.cs
+++ DeleteFiles/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DeleteFiles")]
-[assembly: AssemblyDescription("Small, recursive and date filtered file delete Windows Console utility that can managed deeply nested paths.")]
+[assembly: AssemblyDescription("A File and Folder Delete Utility for the Windows Console that can managed deeply nested paths and also supports data filtering based on dates.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("West Wind Technologies")]
 [assembly: AssemblyProduct("West Wind DeleteFiles")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.10.0.0")]
-[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyVersion("1.11.0.0")]
+[assembly: AssemblyFileVersion("1.11.0.0")]
